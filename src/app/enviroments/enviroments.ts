ea5de8784313ed174aa8ac679999519b9b75bc9b
Oswaldo Gamarra Salinas <131314272+GamaYuto@users.noreply.github.com>
2025-01-19 19:13:03 -0500

    Update

--- src/app/enviroments/enviroments.ts
+++ src/app/enviroments/enviroments.ts
@@ -1,6 +1,4 @@
 export const enviroments = {
     production: false,
-    apiUrl: "https://localhost:7067/api/Clients"
-    
-    //"https://backabc.somee.com/api/Clients"
+    apiUrl: "https://backabc.somee.com/api/Clients"
 }
